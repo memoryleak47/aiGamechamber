15080994ec6810aea9c7d9da03ef8f3333d3a910
memoryleak47 <memoryleak47@protonmail.ch>
2016-05-12 19:11:22 +0200

    worked on rectgame

--- games/rectgame.py
+++ games/rectgame.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import random
+import sys
 
 WIDTH=30
 HEIGHT=30
@@ -10,14 +11,19 @@ RUN=0
 
 class Rectgame:
 	def __init__(self, players):
+		if len(players) != 2:
+			print("Invalid size")
+			sys.exit()
+
 		self.__players=players
 
-		job = CATCH
-		for player in self.__players:
-			player.data.append(job)
-			player.data.append(random.randint(1,WIDTH-1))
-			player.data.append(random.randint(1,HEIGHT-1))
-			job = 1-job
+		self.__players[0].data.append(CATCH)
+		self.__players[1].data.append(RUN)
+
+		self.__players[0].data.append(random.randint(1,WIDTH-1))
+		self.__players[0].data.append(random.randint(1,HEIGHT-1))
+		self.__players[1].data.append(random.randint(1,WIDTH-1))
+		self.__players[1].data.append(random.randint(1,HEIGHT-1))
 	def render(self):
 		field=list()
 
@@ -35,17 +41,18 @@ class Rectgame:
 			print("".join(line))
 
 	def run(self):
-		while True:
+		while not ((self.__players[0].data[1] == self.__players[1].data[1]) and (self.__players[0].data[2]) == (self.__players[1].data[2])):
 			for player in self.__players:
 				self.render()
 				plan = player.act()
 				if plan[0] == -1 and player.data[1] > 1:
 					player.data[1] -= 1
-				elif plan[0] == 1 and player.data[1] < WIDTH-1:
+				elif plan[0] == 1 and player.data[1] < WIDTH-2:
 					player.data[1] += 1
 
 				if plan[1] == -1 and player.data[2] > 1:
 					player.data[2] -= 1
-				elif plan[1] == 1 and player.data[2] < HEIGHT-1:
+				elif plan[1] == 1 and player.data[2] < HEIGHT-2:
 					player.data[2] += 1
 			input("Press any key to continue")
+		print("The game is over")
